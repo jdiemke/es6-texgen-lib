9c8ecea52b11733740d15652a76d7817d82dc5e4
Johannes Diemke <johannesdiemke128@gmail.com>
2016-10-07 22:41:12 +0200

    Adding an entry point for the ECMAScript 6 application.

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -44,10 +44,54 @@
 /* 0 */
 /***/ function(module, exports) {
 
+	/**
+	 * The Application class is used as an entry point for this ECMAScript 6
+	 * application and mimics the Java programming language behavior.
+	 *
+	 * @author jdiemke <johannes.diemke@eventim.de>
+	 * @since 2016-10-07
+	 */
 	
 	'use strict';
 	
-	console.log('Hello World!');
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var Application = function () {
+	    function Application() {
+	        _classCallCheck(this, Application);
+	    }
+	
+	    _createClass(Application, [{
+	        key: 'main',
+	        value: function main() {
+	            this.createAndShowCanvas();
+	        }
+	    }, {
+	        key: 'createAndShowCanvas',
+	        value: function createAndShowCanvas() {
+	            // Create a canvas
+	            var canvas = document.createElement('canvas');
+	            canvas.width = 256;
+	            canvas.height = 256;
+	
+	            // Obtain a rendering context and fill the canvas with black color
+	            var context = canvas.getContext('2d');
+	            context.fillRect(0, 0, 256, 256);
+	
+	            // Add the canvas to the html DOM
+	            document.body.appendChild(canvas);
+	        }
+	    }]);
+	
+	    return Application;
+	}();
+	
+	// Run the application
+	
+	
+	new Application().main();
 
 /***/ }
 /******/ ]);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+/**
+ * The Application class is used as an entry point for this ECMAScript 6
+ * application and mimics the Java programming language behavior.
+ *
+ * @author jdiemke <johannes.diemke@eventim.de>
+ * @since 2016-10-07
+ */
+
 'use strict';
 
-console.log('Hello World!');
+class Application {
+
+    main() {
+        this.createAndShowCanvas();
+    }
+
+    createAndShowCanvas() {
+        // Create a canvas
+        let canvas = document.createElement('canvas');
+        canvas.width = 256;
+        canvas.height = 256;
+
+        // Obtain a rendering context and fill the canvas with black color
+        let context = canvas.getContext('2d');
+        context.fillRect(0, 0, 256, 256);
+
+        // Add the canvas to the html DOM
+        document.body.appendChild(canvas);
+    }
+
+}
+
+// Run the application
+new Application().main();
